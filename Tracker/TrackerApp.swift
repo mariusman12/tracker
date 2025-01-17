a8cdfc980d5dd9ec31db0d437919936f275052e7
Man Marius <manmarius199@gmail.com>
2025-01-17 20:25:23 +0200

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Innovative Sharks on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Innovative Sharks on 17.01.2025.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
